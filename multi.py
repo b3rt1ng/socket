6af94327eb58f5fc52bfe6d875d95def9929a6fb
B3rt1ng <43414019+b3rt1ng@users.noreply.github.com>
2020-02-04 08:10:46 -0500

    Add files via upload

---
+++ multi.py
@@ -0,0 +1,33 @@
+from tkinter import *
+import os
+
+
+def ipfinder():
+	return str(os.popen("ifconfig | grep 192").readlines())[15:-53]
+
+
+def multi():
+	multijoueur = Tk()
+	multijoueur.geometry("300x300")
+	multijoueur.resizable(0,0)
+	multijoueur.title("Multijoueur")
+	try:
+		pseudo
+		existance_pseudo = 1
+	except:
+		existance_pseudo = 0
+	if existance_pseudo == 0:
+		pseudotexte = Label(multijoueur, text="vous n'avez pas de pseudo")
+		pseudo = Entry(multijoueur, width=10)
+
+
+
+	iptexte = Label(multijoueur, text="votre ip locale est: "+ipfinder())
+	iptexte.pack()
+	pseudo.pack()
+	bouton_quitter = Button(multijoueur, text="Quitter", command=multijoueur.quit)
+	bouton_quitter.pack()
+
+	multijoueur.mainloop()
+
+multi()
